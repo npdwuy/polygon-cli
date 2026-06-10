c22280e9b0b5e97a33ad29f5848624c34c5e1ae4
npdwuy <huynguyen030707@gmail.com>
2026-06-10 17:37:20 +0700

    Update

--- polygon.py
+++ polygon.py
@@ -7,17 +7,7 @@ COMMAND = "grant-access"
 # COMMAND = "package-contest"
 
 # 0. Cấu hình BÀI TẬP MỤC TIÊU (Dùng chung cho pipeline và automate-mashup)
-TARGET_SLUGS = [
-    "all-pairs-shortest-path", "ancestor-descendant", "bai-toan-rot-nuoc", "basic-dijkstra", 
-    "boiso", "co-so-doi-xung", "comdiv", "connected-components", "dem-chu-so-khong", 
-    "dem-so-nguyen-to", "dem-uoc-giai-thua", "divsub", "dondep", "island-counting", 
-    "kfusion", "khoang-cach-nguyen-to", "khoi-phuc-cap-so", "luy-thua-modulo", "mult2019", 
-    "ndel", "network-path", "phan-tich-thua-so", "postal-knight", "powmod", "qlnv", 
-    "rect", "sibling-check", "simple-cycle", "sort", "spanning-tree", "sqrnum", "square-maze", 
-    "subtree-size-height", "terrain-path", "tim-gcd-lon-nhat", "tinh-to-hop-chap", 
-    "tong-cap-so-nhan", "tong-so-luong-uoc", "tong-uoc-nho-nhat", "tong-uoc-theo-modulo", 
-    "traffic-toll", "tree-diameter", "xauhople"
-]  # Danh sách slug cụ thể. Để rỗng [] nếu dùng CONTEST_NAME_TO_SYNC
+TARGET_SLUGS = []  # Danh sách slug cụ thể. Để rỗng [] nếu dùng CONTEST_NAME_TO_SYNC
 
 CONTEST_NAME_TO_SYNC = ""  # Nếu có tên, sẽ chạy các problem của contest đó trong contest.json
 
